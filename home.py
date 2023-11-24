9e4543644f9e25431a702ad71bcc5689da81bf3d
Olmira <olmbel@gmail.com>
2023-11-24 13:04:44 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings("ignore")
+
 #import pygame
 st.title('Bienvenue sur notre page festive !')
 
